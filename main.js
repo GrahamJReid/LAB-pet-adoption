c7697159a3151f258c6cdf1966ca0b3b1596cc96
GrahamJReid <grahamreidtechnology@gmail.com>
2022-10-11 21:28:37 -0500

    save2

--- main.js
+++ main.js
@@ -248,7 +248,7 @@ let petColor = document.querySelector("#color");
 petColor.innerHTML += `<div class="card" style="width: 18rem;">
 <div class="card-body">
   <h5 class="card-title">${pets[i].name}</h5>
-  <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+  <h6>${pets[i].specialSkill}</h6>
 <img class="makeitsmall" src=${pets[i].imageUrl}>
 
 </div>
